562103bc666845fd7e9294a157e6f40871143d82
Lepetitdevfr <Jerem71100@gmail.com>
2017-05-05 20:37:54 +0200

    Envoi de mail OK, Debut de connexion mysql

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,5 +1,5 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');     
+var app        = express();               
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 var mysql      = require('mysql');
@@ -24,18 +24,6 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
 
 
 app.get('/articles',function (req,res) {
